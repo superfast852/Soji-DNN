b3af64b87428f8be936d39cf0fd44513e300724d
Gabriel Garcia <superfast852@gmail.com>
2022-08-01 20:26:57 -0400

    Finished PC-Pi communication, added other test files

--- main_socketed.py
+++ main_socketed.py
@@ -9,18 +9,18 @@ import socket
 send_info = True
 debug = True  # Enable Function Outputs
 one = False
-model_type = "v5s"   # Select from: v5s, v5m, v5n, v7, v7t, v5_custom, v7_custom
+model_type = "v7t"   # Select from: v5s, v5m, v5n, v7, v7t, v5_custom, v7_custom
 v5_custom_path = "C:/Users/GG/Desktop/Code/ML/Models/v5s_70/weights/best.pt"  # Path to a YOLOv5 model trained on a custom dataset
 v7_custom_path = "prebuilts/custom/v7t_aqua/V2-512-90%/yolov7.pt"  # Path to a YOLOv7 model trained on a custom dataset
 custom_yaml = "prebuilts/custom/v7t_aqua/data.yaml"  # Path to the custom dataset's data.yaml file
 src = 0  # "Testing/testvid.mp4" or HTTP address
-min_conf = 0.01
+min_conf = 0.5
 pixel_threshold = 25
 avg_fps = []  # List for Calculating Average FPS at ending
 #clr = lambda: (random.randint(0,255), random.randint(0,255), random.randint(0,255))
 clr = lambda: (0, 255, 0)
 record = True
-host = "192.168.197.100"
+host = "localhost"  # "4.tcp.ngrok.io" to use with ngrok
 port = 8220
 
 
@@ -74,9 +74,8 @@ def pos_servo(dets, frame, client, px=25):
     else: yServ = "="
     msg = xServ + " " + yServ
     client.send(msg.encode("utf-8"))
-    while client.recv(2048) != b"ack":
-        print("waiting for ack")
-    print("ack received!")
+    if client.recv(2048) == b"ack":
+        print("ack received!")
 
 def frame_debug(frame, px=25):
     cy, cx = frame.shape[:2]
@@ -125,6 +124,7 @@ if __name__ == "__main__":
 
         # Open Webcam
         vid = cv2.VideoCapture(src)
+        print(f"Connecting to {host}: {port}")
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         client_socket.connect((host, port))
         if record:
--- pi_srvos.py
+++ pi_srvos.py
@@ -1,57 +1,63 @@
-from local_utils.PiStream import stream
+#from local_utils.PiStream import stream
 import multiprocessing
-from adafruit_servokit import ServoKit
-pca = ServoKit(channels=8)
+#from adafruit_servokit import ServoKit
+import socket
+#pca = ServoKit(channels=8)
 servo_angle = [90, 90, 90]
-x = pca.servo[0]
-y1 = pca.servo[1]
-y2 = pca.servo[2]
+#x = pca.servo[0]
+#y1 = pca.servo[1]
+#y2 = pca.servo[2]
 
 
-def servos():
-    import socket
+def servo_ctrl():
+    address = ('localhost', 8220)
 
-    HOST = "192.168.43.56"
-    PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(address)
+    # print(connect(8220, "tcp"))
+    server_socket.listen(5)
+    print("Listening for client . . .")
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
-        s.listen()
-        conn, addr = s.accept()
-        with conn:
-            print(f"Connected by {addr}")
+    while True:
+        conn, addr = server_socket.accept()
+        conn.send(b"ack")
+        print("Connected to client at ", addr)
 
-            while True:
-                data = conn.recv(1024)
-                if not data: break
-                conn.sendall(data)
-                a = data.split(" ")
+        while conn is not None:
+            output = conn.recv(2048)
+            if output.strip() == b"disconnect":
+                print(str(address) + " Disconnected.")
+                conn.close()
+                conn = None
+            elif output:
+                a = output.decode("utf-8").split(" ")
+                conn.send(b"ack")
 
-                for axis in range(len(a)):
-                    if a[axis] == '+':
-                        servo_angle[axis] += 5
-                    elif a[axis] == '-':
-                        servo_angle[axis] -= 5
+            for axis in range(len(a)):
+                if a[axis] == '+':
+                    servo_angle[axis] += 5
+                elif a[axis] == '-':
+                    servo_angle[axis] -= 5
 
-                if a[1] != "=":
-                    b = servo_angle[1] - 90
-                    servo_angle[2] = 90 - b
+            if a[1] != "=":
+                servo_angle[2] = 90 - (servo_angle[1] - 90)
 
-                for i in servo_angle:
-                    if i > 180: i = 180
-                    elif i < 0: i = 0
+            for i in range(3):
+                if servo_angle[i] >= 180: servo_angle[i] = 180
+                elif servo_angle[i] <= 0: servo_angle[i] = 0
 
-                x.angle = servo_angle[0]
-                y1.angle = servo_angle[1]
-                y2.angle = servo_angle[2]
+            #x.angle = servo_angle[0]
+            #y1.angle = servo_angle[1]
+            #y2.angle = servo_angle[2]
+            print(servo_angle)
 
 
-servos = multiprocessing.Process(target=servos)
-stream = multiprocessing.Process(target=stream)
+servos = multiprocessing.Process(target=servo_ctrl)
+#stream = multiprocessing.Process(target=stream)
 
 if __name__ == "__main__":
     servos.start()
-    stream.start()
+    #stream.start()
     servos.join()
-    stream.join()
+    #stream.join()
 
